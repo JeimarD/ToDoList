ee0a4fdc46534e4c0d0e2b2e53da3cfbb1eb9d99
unknown <jeimar1215@gmail.com>
2022-07-28 21:15:19 -0400

    Cambiada la funcionalidad de editar, se paso al componente ToDoContainer, se sigue trabajando en la funcionalidad, cambiado la forma de recibir las tareas

--- src/components/ToDoContainer.jsx
+++ src/components/ToDoContainer.jsx
@@ -3,6 +3,7 @@ import ToDoTasks from "./ToDoTasks";
 
 const ToDoContainer = () => {
   const ref = useRef(null);
+  const [isEditing, setIsEditing] = useState(false);
   const [list, setList] = useState([]);
   const [tasks, setTasks] = useState("");
 
@@ -13,13 +14,32 @@ const ToDoContainer = () => {
   const addTask = (e) => {
     e.preventDefault();
     if (tasks.length === 0) return null;
-    const newToDoList = [...list, tasks];
-    setList(newToDoList);
+    const newTask = { tarea: tasks };
+    setList([...list, newTask]);
+    setTasks("");
     ref.current.value = "";
   };
 
   const handleRemove = (taskName) => {
     setList(list.filter((task) => task !== taskName));
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const editTask = (task) => {
+    const updatedTask = list.map((el) => {
+      if (task === el.tarea) {
+        return {
+          ...el,
+          tarea: "hola",
+        };
+      }
+      return el;
+    });
+    setList(updatedTask);
+    setIsEditing(false);
   };
 
   return (
@@ -36,7 +56,13 @@ const ToDoContainer = () => {
           Add
         </button>
       </div>
-      <ToDoTasks handleRemove={handleRemove} list={list} setList={setList} />
+      <ToDoTasks
+        handleRemove={handleRemove}
+        list={list}
+        handleEdit={handleEdit}
+        isEditing={isEditing}
+        editTask={editTask}
+      />
     </div>
   );
 };
--- src/components/ToDoTasks.jsx
+++ src/components/ToDoTasks.jsx
@@ -1,37 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { AiOutlineEdit, AiOutlineDelete, AiOutlineCheck } from "react-icons/ai";
 
-const ToDoTasks = ({ handleRemove, list, setList }) => {
-  const [isEditing, setIsEditing] = useState(false);
-
-  const handleEdit = () => {
-    setIsEditing(true);
-  };
-
-  const editTask = (task) => {
-    console.log(task);
-    const newUsers = list.map((el) => {
-      if (el === task) {
-        return "xd";
-      }
-      return el;
-    });
-    setList(newUsers);
-    setIsEditing(false);
-  };
-
+const ToDoTasks = ({ handleRemove, list, handleEdit, isEditing, editTask }) => {
   return (
     <>
       {list.map((el) => (
-        <div className="tareas" key={el}>
-          <p>{el}</p>
+        <div className="tareas" key={el.tarea}>
+          <p>{el.tarea}</p>
           {isEditing ? (
             <div className="acciones">
-              <AiOutlineCheck onClick={() => editTask(el)} />
+              <AiOutlineCheck id="editar" onClick={() => editTask(el.tarea)} />
             </div>
           ) : (
             <div className="acciones">
-              <AiOutlineDelete id="eliminar" onClick={() => handleRemove(el)} />
+              <AiOutlineDelete id="eliminar" onClick={() => handleRemove(el.tarea)} />
               <AiOutlineEdit id="editar" onClick={() => handleEdit()} />
             </div>
           )}
